1bc497d870674b401e2b35b4219704fb7b5d7511
GusLM <gustavolimarma@gmail.com>
2026-03-03 13:41:46 -0300

    feat(csv-data-ingestion-jpa): Added a query to the database.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -16,9 +16,14 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
+        // ======== Create the person table if it doesn't exist and insert data. ========
         em.getTransaction().begin();
         personList.forEach(em::persist);
         em.getTransaction().commit();
+
+        // ======== Executes a query and returns the data from the table. ========
+        Person person = em.find(Person.class, 2);
+        System.out.println(person);
 
         em.close();
         emf.close();
